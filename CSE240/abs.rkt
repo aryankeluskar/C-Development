82230e35b234ecb7f2206220705e709597776364
aryankeluskar <aryankeluskar6@gmail.com>
2024-04-01 11:32:20 -0700

    removed classwork

--- CSE240/abs.rkt
+++
@@ -1,8 +0,0 @@
-(define sign
-  (lambda (x)
-    (cond
-      ((> x 0) x)
-      ((= x 0) 0)
-      (else (* x -1)))))
-
-(sign 0)
--- CSE240/var.rkt
+++
@@ -1,6 +0,0 @@
-(let ((a 2)
-      (b 10)
-      (c (read)))
-  (cond
-    ((> (+ a b) c) "good")
-    (else "well")))
